32d5a6a1dddcbd440c78186a831e2e9380eab60e
arctan2 <prateekbm005@gmail.com>
2025-06-25 13:08:10 +0530

    added email-verifier

---
+++ src/fs/image-display.vue
@@ -0,0 +1,69 @@
+<script lang="ts" setup>
+const props = defineProps<{
+	filePath: string,
+	content: string,
+	goBack: () => void
+}>();
+
+</script>
+
+<template>
+	<div class="main-container">
+		<div class="top-bar">
+			<span>{{ props.filePath }}</span>
+			<div @click="props.goBack()" class="close-btn">x</div>
+		</div>
+
+		<div class="image">
+			<img :src="props.content" alt="">
+		</div>
+	</div>
+</template>
+
+<style scoped>
+@import "@css/common.css";
+
+.main-container{
+	display: flex;
+	flex-direction: column;
+	position: relative;
+	height: 100%;
+	overflow-y: hidden;
+}
+
+.image {
+	width: 100%;
+	height: calc(100% - 2rem);
+	transform: translateY(2rem);
+}
+
+.image > img{
+	object-fit: contain;
+	width: 100%;
+	height: 100%;
+}
+
+.top-bar{
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	background-color: white;
+	color: black;
+	font-family: monospace;
+	position: absolute;
+	width: 100%;
+	z-index: 1;
+}
+
+.close-btn{
+	font-weight: bold;
+	background-color: red;
+	color: white;
+	min-height: 1rem;
+	min-width: 1rem;
+	height: 1rem;
+	width: 1rem;
+	text-align: center;
+	cursor: pointer;
+}
+</style>
---
+++ src/fs/text-display.vue
@@ -0,0 +1,96 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = defineProps<{
+	filePath: string,
+	content: string,
+	goBack: () => void
+}>();
+
+const lines = computed(() => props.content.split("\n"));
+
+</script>
+
+<template>
+<div class="main-container">
+	<div class="top-bar">
+		<span>{{ props.filePath }}</span>
+		<div @click="props.goBack()" class="close-btn">x</div>
+	</div>
+
+	<div class="content-container scroll-bar">
+		<div class="line-num">
+			<div v-for="i in lines.length">
+				{{ i }}
+			</div>
+		</div>
+
+		<div class="content">
+			{{ props.content }}
+		</div>
+	</div>
+</div>
+</template>
+
+<style scoped>
+@import "@css/common.css";
+
+.main-container{
+	display: flex;
+	flex-direction: column;
+	height: 100dvh;
+	font-family: monospace;
+}
+
+.content-container{
+	display: flex;
+	flex-direction: row;
+	color: white;
+	font-family: monospace;
+	overflow: auto;
+	white-space: pre;
+	height: 100%;
+	min-width: 100%;
+}
+
+.content {
+	tab-size: 2rem;
+	height: 100%;
+	margin-left: 2ch;
+	width: max-content;
+	min-height: 100%;
+}
+
+.content > div{
+	min-width: max-content;
+	white-space: pre;
+}
+
+.line-num {
+	text-align: right;
+	width: max-content;
+	align-items: center;
+	color: rgb(100, 100, 100);
+}
+
+.top-bar{
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	background-color: white;
+	color: black;
+}
+
+.close-btn{
+	font-weight: bold;
+	background-color: red;
+	color: white;
+	height: 100%;
+	min-height: 1rem;
+	min-width: 1rem;
+	height: 1rem;
+	width: 1rem;
+	text-align: center;
+	cursor: pointer;
+}
+</style>
